a1be47093155b38191bccacb89a620cacf71153b
camilo9898 <cclopez.5400@unicesmag.edu.co>
2025-09-05 13:03:23 -0500

    models2

--- authentication/models.py
+++ authentication/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class User(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20, blank=True)
+
+class Departament(models.Model):
+    name = models.CharField(max_length=20)
+    abrev = models.CharField(max_length=20)
+
