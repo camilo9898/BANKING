02dba2b288d4804b5fa6a9ed2f5b84184fe29e25
camilo9898 <cclopez.5400@unicesmag.edu.co>
2025-09-17 16:20:29 -0500

    migrations

--- authentication/models.py
+++ authentication/models.py
@@ -4,8 +4,45 @@ from django.db import models
 class User(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20, blank=True)
+    mobile_phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    password = models.TextField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
-class Departament(models.Model):
-    name = models.CharField(max_length=20)
-    abrev = models.CharField(max_length=20)
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    abrev = models.CharField(max_length=10, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    abrev = models.CharField(max_length=10, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    abrev = models.CharField(max_length=10, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+User.add_to_class(
+    'city',
+    models.ForeignKey("City", on_delete=models.CASCADE, related_name="users", null=True, blank=True)
+)
+
+City.add_to_class(
+    'department',
+    models.ForeignKey("Department", on_delete=models.CASCADE, related_name="cities", null=True, blank=True)
+)
+
+Department.add_to_class(
+    'country',
+    models.ForeignKey("Country", on_delete=models.CASCADE, related_name="departments", null=True, blank=True)
+)
